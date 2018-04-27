f26d45da635d7b8d5d62aa98431be2b1ff6aca72
Sachi Sharma <sh.sachi90@gmail.com>
2018-04-27 03:12:51 -0400

    Added overlap page

--- src/components/Overlap.js
+++ src/components/Overlap.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react'
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+import _ from 'lodash'
+
 import '../css/Overlap.css'
 import FaHome from 'react-icons/lib/fa/home'
+
+const data = require('../../src/data/overlap.json')
 
 class Overlap extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      elements: []
+      data: []
     };
     this.handleHomeClick = this.handleHomeClick.bind(this);
   }
 
   componentWillMount() {
+    for(var idx in data.overlaps) {
+      this.state.data.push( { "fieldName" : data.overlaps[idx].field,
+                              "targetSystem" : data.overlaps[idx].dest,
+                              "srcSystems" : data.overlaps[idx].sources
+                            });
+    }
   }
 
   componentDidMount() {
@@ -26,6 +38,30 @@ class Overlap extends Component {
     return (
       <div className="overlapWrapper">
         <button className="homeBtn" onClick={this.handleHomeClick} size={70}><FaHome /></button>
+        <ReactTable
+          data={this.state.data}
+          columns={[
+            {
+              Header: "Fields and Overlapping Systems",
+              columns: [
+                {
+                  Header: "Fields",
+                  accessor: "fieldName"
+                },{
+                  Header: "Target System",
+                  accessor: "targetSystem"
+                },{
+                  Header: "Source Systems",
+                  id: "srcSystems",
+                  accessor: d => d.srcSystems
+                }
+              ]
+            }
+          ]}
+          pivotBy={["fieldName"]}
+          defaultPageSize={10}
+          className="-striped -highlight"
+        />
       </div>
     );
   }
